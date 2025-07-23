a5dd81344b5ace449b6fa66ea0af2b678b4f35f5
rahulkumarv269 <rahulkumarv269@gmail.com>
2025-07-23 14:47:11 +0530

    changed Base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+// const BASE_URL = "http://localhost:4000/api/v1"
+
+const BASE_URL = "https://guild-up-frontend-f3m5.vercel.app/api/v1"
 
 export const emailSchedulerEndpoints = {
     EMAIL_SCHEDULAR : BASE_URL + "/emailscheduler/email-scheduler"
